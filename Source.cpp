c8ad8d670a0fcd3c1abc42dbf0ef2771742d19f4
khangduy017 <khangduy017@gmail.com>
2021-09-28 14:18:36 +0700

    Test

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+﻿/*Cho mảng các số nguyên n phần tử được nhập từ bàn phím (mảng này được cấp phát động và quản lý thông qua con trỏ arr), 
+bạn hãy viết chương trình tính tổng của các phần tử trong mảng và hiển thị ra màn hình:*/
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int* a = new int[n];
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	int sum = 0;
+	for (int i = 1; i <= n; i++) {
+		sum += a[i];
+	}
+	cout << sum;
+	delete[] a;
+	return 0;
+}
